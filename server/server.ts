a4e0f04266359063ad9079f9c4fbf1865799e614
asrith-raju <asrithraju143@gmail.com>
2026-01-26 18:04:53 +0530

    server(feat): configure CORS and add session management with MongoDB

--- server/server.ts
+++ server/server.ts
@@ -3,12 +3,35 @@ import cors from 'cors';
 import 'dotenv/config';
 import { connect } from 'node:http2';
 import connectDB from './configs/db.js';
+import MongoStore from 'connect-mongo';
+import session from 'express-session';
+
+declare module 'express-session' {
+    interface SessionData {
+        isLoggedIn: boolean;
+        userId: string;
+    }
+}
 
 await connectDB();
 const app = express();
 
-app.use(cors())
+app.use(cors({
+    origin:['http://localhost:5173','http://localhost:3000'],
+    credentials:true
+}))
+app.use(session({
+    secret: process.env.SESSION_SECRET as string,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 },// 7 days
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI as string,
+        collectionName: 'sessions'
+    })
+}))
 app.use(express.json());
+
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
