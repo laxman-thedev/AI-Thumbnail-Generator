51cc414b60440c8ffdf42515ca7b86efe824009b
asrith-raju <asrithraju143@gmail.com>
2026-01-26 17:41:47 +0530

    server(feat): enhance server setup with CORS and JSON parsing middleware

--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,19 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import { connect } from 'node:http2';
+import connectDB from './configs/db.js';
 
+await connectDB();
 const app = express();
 
-const port = 3000;
+app.use(cors())
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
