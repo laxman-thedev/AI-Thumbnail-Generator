2e64390d81bc8e38a4d44e3946f69acff544eb05
asrith-raju <asrithraju143@gmail.com>
2026-01-28 17:43:27 +0530

    server(feat): define ThumbnailSchema for thumbnail model

--- server/models/Thumbnail.ts
+++ server/models/Thumbnail.ts
@@ -17,3 +17,17 @@ export interface IThumbnail extends Document {
     updatedAt?: Date;
 }
 
+const ThumbnailSchema = new  mongoose.Schema<IThumbnail>({
+     _id: {type:String,ref:'User',required:true},
+    userId: {type:String,ref:'User',required:true},
+    title: {type:String,trim:true,required:true},
+    description: {type:String,trim:true},
+    style:{type:String,required:true,enum:["Bold & Graphic" , "Tech/Futuristic" , "Minimalist" , "Photorealistic" , "Illustrated"]},
+    aspect_ratio: {type:String,required:true,enum:["16:9" , "1:1" ,"9:16"],default:"16:9"},
+    color_scheme:{type:String,required:true,enum:["vibrant" , "sunset" , "forest" ,"neon" ,"purple" ,"monochrome" , "ocean" , "pastel"]},
+    text_overlay:{ type:Boolean,default:false},
+    image_url: {type:String,default:" "},
+    prompt_used: {type:String},
+    user_prompt: {type:String},
+    isGenerating:{type:Boolean,default:true} 
+})
