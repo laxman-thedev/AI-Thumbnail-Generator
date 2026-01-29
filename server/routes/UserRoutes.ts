94ba3b8a0c780a6b27f50cf896a7db9a551a9f9b
asrith-raju <asrithraju143@gmail.com>
2026-01-29 18:37:32 +0530

    server(feat): add authentication middleware to thumbnail and user routes

--- server/routes/UserRoutes.ts
+++ server/routes/UserRoutes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import { getThumbnailById, getUserThumbnails } from '../controllers/UserController.js';
+import protect from '../middlewares/auth.js';
 
 
 const UserRouter = express.Router();    
 
-UserRouter.get('/thumbnails', getUserThumbnails);
-UserRouter.get('/thumbnails/:id', getThumbnailById);
+UserRouter.get('/thumbnails',protect, getUserThumbnails);
+UserRouter.get('/thumbnails/:id',protect ,getThumbnailById);
 
 export default UserRouter;
