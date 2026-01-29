f2832f449937ca08b22d167bcc9860e088a2549e
asrith-raju <asrithraju143@gmail.com>
2026-01-29 12:13:40 +0530

    server(feat): create UserRouter for handling thumbnail routes

---
+++ server/routes/UserRoutes.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import { getThumbnailById, getUserThumbnails } from '../controllers/UserController.js';
+
+
+const UserRouter = express.Router();    
+
+UserRouter.get('/thumbnails', getUserThumbnails);
+UserRouter.get('/thumbnails/:id', getThumbnailById);
+
+export default UserRouter;
